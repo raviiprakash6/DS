7671d0bfa2d49e497d3ff08d6ef178e0f0f293e9
rprakash <rprakash@xactlyycorp.com>
2021-08-12 21:35:56 +0530

    Added BFS

---
+++ venv/DataStructure/Graph/adjacencyList.py
@@ -0,0 +1,42 @@
+class AdjNode:
+    def __init__(self, data):
+        self.vertex = data
+        self.next = None
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [None] * self.V
+
+    def add_edge(self, source, destination):
+        # Adding edge to source node
+        node = AdjNode(destination)
+        node.next = self.graph[source]
+
+        # Adding source node to destination in case of undirected graph
+        node = AdjNode(source)
+        node.next = self.graph[destination]
+        self.graph[destination] = node
+
+    # Print the graph
+    def print_graph(self):
+        for i in range(self.V):
+            print("Vertex " + str(i) + ":", end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+            print(" \n")
+
+
+if __name__ == "__main__":
+    V = 5
+
+    # Create graph and edges
+    graph = Graph(V)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(0, 3)
+    graph.add_edge(1, 2)
+    graph.print_graph()
