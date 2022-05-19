80362a172a8bddc39a3a3f73c21e472c7b8b37ae
rprakash <rprakash@xactlyycorp.com>
2022-05-19 21:53:06 +0530

    Added efficient soln of maxCircularSubarray

--- venv/DataStructure/Array/maxCircularSubarray.py
+++ venv/DataStructure/Array/maxCircularSubarray.py
@@ -12,5 +12,24 @@ def maxCircularSubarray(arr):
 
     return maxsum
 
+arr=[5,-2,3,4]
+print(maxCircularSubarray(arr))
 
-print(maxCircularSubarray([1, 6, -6, 3, 4,-4, 1]))
+
+
+##########################################################
+def maxCircularSubarrayEfficientSoln(arr):
+    presum=0
+    minsum=0
+    sum=0
+    for i in range(0,len(arr)):
+        sum+=arr[i]
+        presum=min(arr[i],arr[i]+presum)
+        minsum=min(minsum,presum)
+
+    return sum-minsum
+
+
+print(maxCircularSubarrayEfficientSoln(arr))
+
+
