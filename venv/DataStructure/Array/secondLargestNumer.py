00336b1b4bf723fbe8d871247a50ff771b8091db
rprakash <rprakash@xactlyycorp.com>
2022-05-19 20:35:23 +0530

    Second largest number

--- venv/DataStructure/Array/secondLargestNumer.py
+++ venv/DataStructure/Array/secondLargestNumer.py
@@ -1,16 +1,15 @@
 def secondLargestNumber(arr):
     n = len(arr)
-    large=arr[0]
-    secondLarge=-1
-    for i in range(1,n):
-        if arr[i]>large:
-            secondLarge=large
-            large=arr[i]
-        elif arr[i] < large and arr[i] > secondLarge :
-            secondLarge=arr[i]
+    large = arr[0]
+    secondLarge = -1
+    for i in range(1, n):
+        if arr[i] > large:
+            secondLarge = large
+            large = arr[i]
+        elif arr[i] < large and arr[i] > secondLarge:
+            secondLarge = arr[i]
     return secondLarge
 
-arr=[12, 35, 1, 10, 34, 1e]
+
+arr = [12, 35, 1, 10, 34, 1]
 print(secondLargestNumber(arr))
-
-
