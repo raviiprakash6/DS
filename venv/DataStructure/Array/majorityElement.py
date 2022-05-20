6b15b977f7f60086dcf2ea74b8dd88d51f0250ad
rprakash <rprakash@xactlyycorp.com>
2022-05-20 09:13:55 +0530

    Added efficient soln of majorityElement

--- venv/DataStructure/Array/majorityElement.py
+++ venv/DataStructure/Array/majorityElement.py
@@ -1,41 +1,47 @@
 def majorityElement(arr):
     """A majority element in an array A[] of size n is an element that
        appears more than n/2 times (and hence there is at most one such element)"""
-
-    i=arr[0]
-    n=len(arr)
-    count=1
-    for j in range(1,len(arr)):
-        if(arr[j]==arr[j-1]):
-            count=count+1
-            elem=arr[j]
+    Elem=-1
+    count = 1
+    count1 = 0
+    elemIndex = 0
+    n = len(arr)
+    for j in range(1, n):
+        if arr[j] == arr[j - 1]:
+            count = count + 1
         else:
-            count=count-1
-    if(count>0):
-        print(elem)
-    else:
-        print(False)
+            count = count - 1
+
+        if count == 0:
+            elemIndex = j
+            count = 1
+
+    if count > 0:
+        for i in range(0, n):
+            if arr[i] == arr[j]:
+                count1 += 1
+        if count1 > n / 2:
+            Elem = arr[i]
+    return print(Elem)
 
 
-arr=[2,2,2,2,3,3,3]
+
+arr = [6,8,7,6,6]
 majorityElement(arr)
 
 
-
 #####################Naive Approach####################
-# def majorityElement(arr):
-#
-#     Elem=-1
-#     n=len(arr)
-#     for i in range(n):
-#         count=0
-#         for j in range(i,n):
-#           if(arr[i]==arr[j]):
-#               count+=1
-#         if(count > n/2):
-#             Elem=arr[i]
-#     return  print(Elem)
-#
-# arr=[2,2,2,2,3,3,3]
-# majorityElement(arr)
+def majorityElementNaiveSoln(arr):
+    Elem = -1
+    n = len(arr)
+    for i in range(n):
+        count = 0
+        for j in range(i, n):
+            if (arr[i] == arr[j]):
+                count += 1
+        if (count > n / 2):
+            Elem = arr[i]
+    return print(Elem)
 
+
+majorityElementNaiveSoln(arr)
