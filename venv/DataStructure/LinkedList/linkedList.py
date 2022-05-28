30805bbe995ce19fe7698b096f801aeb17c5648a
rprakash <rprakash@xactlyycorp.com>
2022-05-28 15:54:42 +0530

    reformatted

--- venv/DataStructure/LinkedList/linkedList.py
+++ venv/DataStructure/LinkedList/linkedList.py
@@ -1,7 +1,8 @@
 class Node:
-    def __init__(self,data=None,next=None):
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
+
 
 class LinkedList:
     def __init__(self):
@@ -11,28 +12,27 @@ class LinkedList:
         if self.head is None:
             print("List is empty")
 
-        itr=self.head
-        llstr=''
+        itr = self.head
+        llstr = ''
         while itr:
             llstr += str(itr.data) + ' --> '
-            itr=itr.next
+            itr = itr.next
         print(llstr)
 
-    def insertAtBeginning(self,data):
+    def insertAtBeginning(self, data):
         node = Node(data, self.head)
-        self.head=node
+        self.head = node
 
-
-    def insertAtEnd(self,data):
+    def insertAtEnd(self, data):
 
         if self.head is None:
-            self.head=Node(data)
+            self.head = Node(data)
             return
 
-        itr=self.head
+        itr = self.head
         while itr.next:
-            itr=itr.next
-        itr.next=Node(data,None)
+            itr = itr.next
+        itr.next = Node(data, None)
 
     def getLength(self):
         count = 0
@@ -42,44 +42,41 @@ class LinkedList:
             itr = itr.next
         return count
 
-    def insertAt(self,index,data):
-        if(index==0):
-            insertAtBegining(data)
-        if(index==self.getLength()):
-            insertAtEnd(data)
+    def insertAt(self, index, data):
+        if index == 0:
+            self.insertAtBeginning(data)
+        if index == self.getLength():
+            self.insertAtEnd(data)
 
-        count=0
-        itr=self.head
+        count = 0
+        itr = self.head
         while itr:
-            if(count==index-1):
-                node=Node(data,itr.next)
-                itr.next=node
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
                 break
-            itr=itr.next
-            count=count+1
+            itr = itr.next
+            count = count + 1
 
     def removeAtBeginning(self):
-         itr=self.head
-         self.head=itr.next
+        itr = self.head
+        self.head = itr.next
 
     def removeAtEnd(self):
         itr = self.head
-        while(itr.next.next):
-            itr=itr.next
-        itr.next=None
+        while (itr.next.next):
+            itr = itr.next
+        itr.next = None
 
-
-    def removeAt(self,index):
-        itr=self.head
-        count=0
+    def removeAt(self, index):
+        itr = self.head
+        count = 0
         while itr:
-            if(count==index-1):
-                itr.next=itr.next.next
+            if count == index - 1:
+                itr.next = itr.next.next
                 break
-            itr=itr.next
-            count+=1
-
-
+            itr = itr.next
+            count += 1
 
 
 if __name__ == '__main__':
@@ -88,7 +85,7 @@ if __name__ == '__main__':
     ll.insertAtBeginning(5)
     ll.insertAtEnd(87)
     ll.insertAtEnd(98)
-    ll.insertAt(1,80)
+    ll.insertAt(1, 80)
     ll.print()
     ll.removeAtBeginning()
     ll.print()
@@ -96,8 +93,3 @@ if __name__ == '__main__':
     ll.print()
     ll.removeAt(2)
     ll.print()
-
-
-
-
-
